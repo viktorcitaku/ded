8ab942adf8f4495f6114eb6b0d880ff6968b7c4b
Viktor Çitaku <viktor.citaku@gmail.com>
2020-09-21 01:59:28 +0200

    Dockerfile

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 # The JVM_ARGS should not be wrapped with quotes otherwise it will lead to: cannot load main class error
 # shellcheck disable=SC2068
-exec java -XX:MaxRAMPercentage="${MEM_MAX_RAM_PERCENTAGE}" -Xss"${MEM_XSS}" -XX:+UseContainerSupport ${JVM_ARGS} -jar payara-micro.jar --port "${PORT}" "$@"
+exec java -XX:MaxRAMPercentage="${MEM_MAX_RAM_PERCENTAGE}" -Xss"${MEM_XSS}" -XX:+UseContainerSupport ${JVM_ARGS} -jar payara-micro.jar "$@"
