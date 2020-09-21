aa65dd8f613af3f230f5a252a1bdafd5c4387a3f
Viktor Çitaku <viktor.citaku@gmail.com>
2020-09-21 12:55:27 +0200

    update Heap and Stack size allocation to make it friendlier for Heroku

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 # The JVM_ARGS should not be wrapped with quotes otherwise it will lead to: cannot load main class error
 # shellcheck disable=SC2068
 # exec java -XX:MaxRAMPercentage="${MEM_MAX_RAM_PERCENTAGE}" -Xss"${MEM_XSS}" -XX:+UseContainerSupport ${JVM_ARGS} -jar payara-micro.jar "$@"
-exec java -XX:MaxRAMPercentage="${MEM_MAX_RAM_PERCENTAGE}" -Xss"${MEM_XSS}" -XX:+UseContainerSupport ${JVM_ARGS} \
+exec java -Xmx"${MEM_MAX_HEAP}" -Xss"${MEM_XSS}" -XX:+UseContainerSupport ${JVM_ARGS} \
   -jar payara-micro.jar --deploydir /opt/payara/deployments --nocluster --contextroot ded --postbootcommandfile post-boot-commands.asadmin --port ${PORT}
